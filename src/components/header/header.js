cb95afc2ad2f0388c768ccee0c43f3a7f2979931
Pascal <pascaladnan101@gmail.com>
2023-12-07 12:32:00 +0700

    Add sticky header, change logo when sticky

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,35 @@
-import Logo from "../icons/logo";
-import "./header.css";
-import ArrowDown from "../icons/arrow-down";
+"use client";
 
+import React, { useState, useEffect } from 'react';
+import Logo from '../icons/logo';
+import './header.css';
+import ArrowDown from '../icons/arrow-down';
 
 const Header = () => {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const threshold = 50;
+      setIsSticky(scrollPosition > threshold);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <section className="w-full header">
+    <section className={`w-full header${isSticky ? ' sticky' : ''}`}>
       <div className="flex items-center">
-        <Logo width={56} height={72}/>
-        <div className="font-logo">stasec</div>
+      <Logo
+          width={150}
+          height={72}
+          isSticky={isSticky}
+        />
       </div>
       <div className="header-options">
         <div className="home-btn">HOME</div>
