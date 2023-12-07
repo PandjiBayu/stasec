c4c944515af4ab840bd8d54eaef7ec1d8589fa29
Pascal <pascaladnan101@gmail.com>
2023-12-07 12:42:57 +0700

    fix link navbar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,18 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
-import Logo from '../icons/logo';
-import './header.css';
-import ArrowDown from '../icons/arrow-down';
+import React, { useState, useEffect } from "react";
+import Logo from "../icons/logo";
+import "./header.css";
+import ArrowDown from "../icons/arrow-down";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
   const [isSticky, setIsSticky] = useState(false);
-
+  const route = usePathname();
+  const isActive = (path) => {
+    return route === path;
+  };
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
@@ -15,29 +20,37 @@ const Header = () => {
       setIsSticky(scrollPosition > threshold);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
-    <section className={`w-full header${isSticky ? ' sticky' : ''}`}>
+    <section className={`w-full header${isSticky ? " sticky" : ""}`}>
       <div className="flex items-center">
-      <Logo
-          width={150}
-          height={72}
-          isSticky={isSticky}
-        />
+        <Logo width={150} height={72} isSticky={isSticky} />
       </div>
       <div className="header-options">
-        <div className="home-btn">HOME</div>
+        <Link
+          href={"/"}
+          className={isActive("/") ? "current-btn" : ""}
+          style={{ padding: "10px 24px" }}
+        >
+          HOME
+        </Link>
         <div className="flex items-center space-x-1">
           <div>SERVICE</div>
           <ArrowDown />
         </div>
-        <div>INDUSTRY</div>
+        <Link
+          href={"/industry"}
+          className={isActive("/industry") ? "current-btn" : ""}
+          style={{ padding: "10px 24px" }}
+        >
+          INDUSTRY
+        </Link>
         <div>ABOUT</div>
       </div>
       <div className="contactus-btn">CONTACT US</div>
