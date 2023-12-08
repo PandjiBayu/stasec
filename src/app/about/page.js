75af62cfb1231565de408e912f3b642ed9b10963
Pascal <pascaladnan101@gmail.com>
2023-12-08 12:36:00 +0700

    fix tab in about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Header from "@/components/header/header";
 import Footer from "@/components/footer/footer";
 import HeadVector from "@/components/icons/head-vector";
@@ -5,9 +7,16 @@ import Testimonial from "@/components/testimonial/testimonial";
 import Image from "next/image";
 import Marquee from "@/components/marquee/marquee";
 import { images } from "@/data/marquee/marqueeData";
+import { useState } from "react";
 
 export default function About() {
   const imagesRepeated = images.concat(images, images, images);
+  const [activeTab, setActiveTab] = useState("vision");
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
     <section className="main">
       <div className="gradient-image"></div>
@@ -29,24 +38,58 @@ export default function About() {
             <div className="flex flex-col space-y-12">
               <div className="content-title">A Stellar Path to Security</div>
               <div className="text-[18px]">
-              STASEC is the amalgamation of two fundamental concepts: “Stellar” 
-and “Security”. Just as stars light up the night sky and act as a guilding beacons. We help  you embarking on a journey towards a more secure, resilient, and thriving digital future. STASEC is registered and operates under PT. Digital Berkah Mandiri.
+                STASEC is the amalgamation of two fundamental concepts:
+                “Stellar” and “Security”. Just as stars light up the night sky
+                and act as a guilding beacons. We help you embarking on a
+                journey towards a more secure, resilient, and thriving digital
+                future. STASEC is registered and operates under PT. Digital
+                Berkah Mandiri.
               </div>
             </div>
           </div>
           <div className="flex items-center space-x-[250px]">
             <div className="flex flex-col space-y-6">
               <div className="fpoint-text text-[24px] flex">
-                <div className="flex-1 w-full border-b-2">Our Vision</div>
-                <div className="border-b-2 border-[#366BE9] flex-1">
-                  Our Mission
+                <div
+                  className={`flex-1 border-b-2 ${
+                    activeTab === "vision"
+                      ? "border-[#366BE9] text-blue-500"
+                      : ""
+                  }`}
+                  onClick={() => handleTabClick("vision")}
+                >
+                  <span
+                    className={`inline-block w-full ${
+                      activeTab === "vision" ? "text-blue-500" : ""
+                    }`}
+                  >
+                    Our Vision
+                  </span>
                 </div>
-                <div className="flex-1 w-full border-b-2"></div>
+                <div
+                  className={`border-b-2 flex-1 ${
+                    activeTab === "mission"
+                      ? "border-[#366BE9] text-blue-500"
+                      : ""
+                  }`}
+                  onClick={() => handleTabClick("mission")}
+                >
+                  <span
+                    className={`inline-block w-full ${
+                      activeTab === "mission" ? "text-blue-500" : ""
+                    }`}
+                  >
+                    Our Mission
+                  </span>
+                </div>
+                <div className="flex-1 border-b-2"></div>
               </div>
               <div className="text-[18px]">
-                Providing cybersecurity integrated solutions for comprehensive,
-                integrative, and adaptive protection of digital assets against
-                evolving threats.
+                {activeTab === "vision"
+                  ? // Content for Vision tab
+                    "Become a leading company in the field of cybersecurity by delivering innovative solutions that empower businesses to grow securely in the digital era."
+                  : // Content for Mission tab
+                    "Providing cybersecurity integrated solutions for comprehensive, integrative, and adaptive protection of digital assets against evolving threats."}
               </div>
             </div>
             <div>
