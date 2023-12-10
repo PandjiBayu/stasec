0155f0f83fa08d32ee64c478119940294742ce4b
Pascal <pascaladnan101@gmail.com>
2023-12-10 22:12:18 +0700

    line vector service-2

--- src/app/service-2/page.js
+++ src/app/service-2/page.js
@@ -22,19 +22,24 @@ import { workProcess2 } from "@/data/service/workProcess";
 import FooterContainer from "@/components/footer/footer-container";
 import BannerService2 from "@/components/banner-homepage/banner-service-2";
 
-
 export default function Service2() {
   return (
-    <section  >
-     <BannerService2 />
+    <section className="relative overflow-x-hidden">
+      <BannerService2 />
       <div className="md:service2-bg-1 flex items-center justify-center mt-[-50px]">
         <Features {...serviceFeatures2} />
       </div>
-      
-      <div className="flex items-center md:service2-bg-2 my-14 ">
-
+      <Image
+        className="absolute hidden line-vector-1 lg:block"
+        src="/line-right.svg"
+        width={480}
+        height={480}
+        alt="head-vector"
+        style={{ top: "1000px" }}
+      />
+      <div className="flex items-center mt-64 md:service2-bg-2 my-14">
         <div className="grid items-start grid-cols-1 px-3 md:grid-cols-2 lg:items-center md:space-x-8 md:px-12 lg:px-20 sm:px-6">
-          <div  >
+          <div>
             <Image
               src="/service2-content.png"
               width={1200}
@@ -45,9 +50,7 @@ export default function Service2() {
           </div>
 
           <div className="flex flex-col items-center mt-5 sm:items-start sm:mt-0">
-            <div
-              className="fpoint-text font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl pb-[24px]"
-            >
+            <div className="fpoint-text font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl pb-[24px]">
               The Key Benefit Of Our Service
             </div>
             <div className="text-[18px]">
@@ -62,42 +65,42 @@ export default function Service2() {
               className="pt-[72px] grid grid-cols-1 sm:grid-cols-2 gap-5"
               style={{ lineHeight: "32px" }}
             >
-                  <div className="card flex flex-col items-center space-y-[12px] p-3 lg:p-[32px]  justify-center min-h-[248px] sm:h-auto">
+              <div className="card flex flex-col items-center space-y-[12px] p-3 lg:p-[32px]  justify-center min-h-[248px] sm:h-auto">
                 <div>
                   <Customize width={64} height={64} />
                 </div>
                 <h4
-                 className="text-xl fpoint-text lg:text-2xl"
+                  className="text-xl fpoint-text lg:text-2xl"
                   style={{ textAlign: "center" }}
                 >
                   Reduces the risk of security vulnerabilities in the software
                 </h4>
               </div>
-              
+
               <div className="card flex flex-col items-center space-y-[12px] p-3 lg:p-[32px]  justify-center min-h-[248px] sm:h-auto">
                 <div>
                   <ShieldCheck width={64} height={64} color={"white"} />
                 </div>
                 <h4
-                className="text-xl fpoint-text lg:text-2xl"
+                  className="text-xl fpoint-text lg:text-2xl"
                   style={{ textAlign: "center" }}
                 >
                   Ensures that security is a part of development process
                 </h4>
               </div>
-              
+
               <div className="card flex flex-col items-center space-y-[12px] p-3 lg:p-[32px]  justify-center min-h-[248px] sm:h-auto">
                 <div>
                   <DollarSign width={64} height={64} color={"white"} />
                 </div>
                 <h4
-                className="text-xl fpoint-text lg:text-2xl"
+                  className="text-xl fpoint-text lg:text-2xl"
                   style={{ textAlign: "center" }}
                 >
                   Reduce the need for costly post-development security fixes
                 </h4>
               </div>
-              
+
               <div className="card flex flex-col items-center space-y-[12px] p-3 lg:p-[32px]  justify-center min-h-[248px] sm:h-auto">
                 <div>
                   <Award width={64} height={64} color={"white"} />
@@ -109,21 +112,23 @@ export default function Service2() {
                   Enhances overall software quality
                 </h4>
               </div>
-
             </div>
-            
           </div>
-          
         </div>
-
+      </div>
+      <Image
+          className="absolute hidden line-vector-2 lg:block"
+          src="/line-left.svg"
+          width={480}
+          height={480}
+          alt="head-vector"
+          style={{ top: "2400px" }}
+        />
+      <div className="flex items-center mt-64 md:service2-bg-3">
+        <Features {...workProcess2} />
       </div>
 
-      <div className="flex items-center md:service2-bg-3">
-      <Features {...workProcess2} />
-      </div>
- 
       <FooterContainer />
-
     </section>
   );
 }
