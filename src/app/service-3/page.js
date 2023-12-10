90ca15239e2ec9e8cc9ca3b77c8722c9fb5cbed1
Pascal <pascaladnan101@gmail.com>
2023-12-10 22:15:07 +0700

    line vector service 3

--- src/app/service-3/page.js
+++ src/app/service-3/page.js
@@ -1,9 +1,7 @@
 import Header from "@/components/header/header";
 import Features from "@/components/features/features";
 import Footer from "@/components/footer/footer";
-import {
-  serviceFeatures3,
-} from "@/data/service/serviceFeatures";
+import { serviceFeatures3 } from "@/data/service/serviceFeatures";
 import Customize from "@/components/icons/customize";
 import ShieldCheck from "@/components/icons/shield-check";
 import DollarSign from "@/components/icons/dollar-sign";
@@ -15,104 +13,112 @@ import BannerService3 from "@/components/banner-homepage/banner-service-3";
 
 export default function Service3() {
   return (
-    <section>
-     <BannerService3 />
+    <section className="relative overflow-x-hidden">
+      <BannerService3 />
       <div className="service2-bg-1 flex items-center justify-center mt-[-50px]">
         <Features {...serviceFeatures3} />
       </div>
+      <Image
+        className="absolute hidden line-vector-1 lg:block"
+        src="/line-right.svg"
+        width={480}
+        height={480}
+        alt="head-vector"
+        style={{ top: "1000px" }}
+      />
+      <div className="flex items-center mt-64 md:service2-bg-2 my-14">
+        <div className="grid items-start grid-cols-1 gap-6 px-3 md:grid-cols-2 md:gap-1 lg:gap-16 lg:items-center md:space-x-8 md:px-12 lg:px-20 sm:px-6">
+          <div>
+            <Image
+              src="/service3-content.png"
+              width={1200}
+              height={1200}
+              alt="feature-point"
+              className="rounded-xl"
+            />
+          </div>
 
-      <div className="flex items-center md:service2-bg-2 my-14 ">
+          <div className="flex flex-col items-start mt-5 sm:mt-0">
+            <div className="fpoint-text font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl pb-[24px]">
+              The Key Benefit Of Our Service
+            </div>
+            <div className="text-[18px]">
+              Introducing Security Development Tools, your key to fortified
+              digital resilience. Enhance your cybersecurity posture with
+              advanced tools designed to streamline security development,
+              reducing vulnerabilities and ensuring robust protection against
+              evolving threats.
+            </div>
 
-<div className="grid items-start grid-cols-1 gap-6 px-3 md:grid-cols-2 md:gap-1 lg:gap-16 lg:items-center md:space-x-8 md:px-12 lg:px-20 sm:px-6">
-  <div  >
-    <Image
-      src="/service3-content.png"
-      width={1200}
-      height={1200}
-      alt="feature-point"
-      className="rounded-xl"
-    />
-  </div>
+            <div
+              className="pt-[72px] grid grid-cols-1 sm:grid-cols-2 gap-5"
+              style={{ lineHeight: "32px" }}
+            >
+              <div className="card flex flex-col items-center space-y-[12px] p-3 lg:p-[32px]  justify-center min-h-[248px] sm:h-auto">
+                <div>
+                  <Customize width={64} height={64} />
+                </div>
+                <h4
+                  className="text-xl fpoint-text lg:text-2xl"
+                  style={{ textAlign: "center" }}
+                >
+                  Boosts software security with active threat monitoring and
+                  response
+                </h4>
+              </div>
 
-  <div className="flex flex-col items-start mt-5 sm:mt-0">
-    <div
-      className="fpoint-text font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl pb-[24px]"
-    >
-      The Key Benefit Of Our Service
-    </div>
-    <div className="text-[18px]">
-      Introducing Security Development Tools, your key to fortified
-      digital resilience. Enhance your cybersecurity posture with
-      advanced tools designed to streamline security development,
-      reducing vulnerabilities and ensuring robust protection against
-      evolving threats.
-    </div>
+              <div className="card flex flex-col items-center space-y-[12px] p-3 lg:p-[32px]  justify-center min-h-[248px] sm:h-auto">
+                <div>
+                  <ShieldCheck width={64} height={64} color={"white"} />
+                </div>
+                <h4
+                  className="text-xl fpoint-text lg:text-2xl"
+                  style={{ textAlign: "center" }}
+                >
+                  Reduces the likelihood of data breaches and security incidents
+                </h4>
+              </div>
 
-    <div
-      className="pt-[72px] grid grid-cols-1 sm:grid-cols-2 gap-5"
-      style={{ lineHeight: "32px" }}
-    >
-          <div className="card flex flex-col items-center space-y-[12px] p-3 lg:p-[32px]  justify-center min-h-[248px] sm:h-auto">
-        <div>
-          <Customize width={64} height={64} />
+              <div className="card flex flex-col items-center space-y-[12px] p-3 lg:p-[32px]  justify-center min-h-[248px] sm:h-auto">
+                <div>
+                  <DollarSign width={64} height={64} color={"white"} />
+                </div>
+                <h4
+                  className="text-xl fpoint-text lg:text-2xl"
+                  style={{ textAlign: "center" }}
+                >
+                  Reduces downtime and financial losses from cyber attacks
+                </h4>
+              </div>
+
+              <div className="card flex flex-col items-center space-y-[12px] p-3 lg:p-[32px]  justify-center min-h-[248px] sm:h-auto">
+                <div>
+                  <Award width={64} height={64} color={"white"} />
+                </div>
+                <h4
+                  className="text-xl fpoint-text lg:text-2xl"
+                  style={{ textAlign: "center" }}
+                >
+                  Supports compliance with security regulations and standards
+                </h4>
+              </div>
+            </div>
+          </div>
         </div>
-        <h4
-         className="text-xl fpoint-text lg:text-2xl"
-          style={{ textAlign: "center" }}
-        >
-       Boosts software security with active threat monitoring and response
-        </h4>
       </div>
-      
-      <div className="card flex flex-col items-center space-y-[12px] p-3 lg:p-[32px]  justify-center min-h-[248px] sm:h-auto">
-        <div>
-          <ShieldCheck width={64} height={64} color={"white"} />
-        </div>
-        <h4
-        className="text-xl fpoint-text lg:text-2xl"
-          style={{ textAlign: "center" }}
-        >
-       Reduces the likelihood of data breaches and security incidents
-        </h4>
-      </div>
-      
-      <div className="card flex flex-col items-center space-y-[12px] p-3 lg:p-[32px]  justify-center min-h-[248px] sm:h-auto">
-        <div>
-          <DollarSign width={64} height={64} color={"white"} />
-        </div>
-        <h4
-        className="text-xl fpoint-text lg:text-2xl"
-          style={{ textAlign: "center" }}
-        >
-    Reduces downtime and financial losses from cyber attacks
-        </h4>
-      </div>
-      
-      <div className="card flex flex-col items-center space-y-[12px] p-3 lg:p-[32px]  justify-center min-h-[248px] sm:h-auto">
-        <div>
-          <Award width={64} height={64} color={"white"} />
-        </div>
-        <h4
-          className="text-xl fpoint-text lg:text-2xl"
-          style={{ textAlign: "center" }}
-        >
-        Supports compliance with security regulations and standards
-        </h4>
-      </div>
-
-    </div>
-    
-  </div>
-  
-</div>
-
-</div>
-
-      <div className="flex items-center md:service2-bg-3">
+      <Image
+          className="absolute hidden line-vector-2 lg:block"
+          src="/line-left.svg"
+          width={480}
+          height={480}
+          alt="head-vector"
+          style={{ top: "2400px" }}
+        />
+      <div className="flex items-center mt-64 md:service2-bg-3">
         <Features {...workProcess3} />
       </div>
-    
-    <FooterContainer />
+
+      <FooterContainer />
     </section>
   );
 }
