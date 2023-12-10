2e5147e921b71a37c3fd6647d8f19b7ff23e41d7
Pascal <pascaladnan101@gmail.com>
2023-12-10 18:12:03 +0700

    Update banner service 1

--- src/components/banner-homepage/banner-service-1.js
+++ src/components/banner-homepage/banner-service-1.js
@@ -9,7 +9,7 @@ const BannerService1 = () => {
       <div className="w-full px-5 sm:px-[80px] flex sm:pt-[80px]">
           <div className="flex flex-col space-y-[40px] py-[80px]">
             <div className="header-title">Penetration Test</div>
-            <div className="font-[16px]">
+            <div className="font-[16px] w-full md:w-1/2">
               It&apos;s a comprehensive services that help you identify
               vulnerabilities in your systems and applications, enabling you to
               proactively strengthen your defenses.
