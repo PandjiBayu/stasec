651c8b7b82a6980bb23d60cd9aada15b52859c02
Pascal <pascaladnan101@gmail.com>
2023-12-11 10:43:06 +0700

    make headline hero in center

--- src/components/banner-homepage/banner-service-1.js
+++ src/components/banner-homepage/banner-service-1.js
@@ -1,29 +1,26 @@
-const { default: Header } = require("../header/header")
-const { default: HeadVector } = require("../icons/head-vector")
+const { default: Header } = require("../header/header");
+const { default: HeadVector } = require("../icons/head-vector");
 
 const BannerService1 = () => {
-
-    return(
- <div class="head-vector service1-image bg-cover h-screen ">
+  return (
+    <div class="head-vector service1-image bg-cover h-screen ">
       <Header />
       <div className="w-full px-5 sm:px-[80px] flex sm:pt-[80px]">
-          <div className="flex flex-col space-y-[40px] py-[80px]">
-            <div className="header-title">Penetration Test</div>
-            <div className="font-[16px] w-full md:w-1/2">
-              It&apos;s a comprehensive services that help you identify
-              vulnerabilities in your systems and applications, enabling you to
-              proactively strengthen your defenses.
-            </div>
-            <div className="get-started">
-              <div className="get-started-font">Get Started</div>
-            </div>
+        <div className="flex flex-col space-y-[40px] py-[80px] items-center md:items-start">
+          <div className="text-center header-title md:text-start">Penetration Test</div>
+          <div className="font-[16px] w-full md:w-1/2 text-center md:text-start">
+            It&apos;s a comprehensive services that help you identify
+            vulnerabilities in your systems and applications, enabling you to
+            proactively strengthen your defenses.
           </div>
-          <div className="hidden md:w-full"></div>
+          <div className="get-started">
+            <div className="get-started-font">Get Started</div>
+          </div>
         </div>
+        <div className="hidden md:w-full"></div>
       </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default BannerService1
- 
+export default BannerService1;
--- src/components/banner-homepage/banner-service-2.js
+++ src/components/banner-homepage/banner-service-2.js
@@ -3,11 +3,11 @@ import Header from "../header/header"
 const BannerService2 = () => {
 
   return(
-    <div className="head-vector service2-image bg-cover h-screen ">
+    <div className="h-screen bg-cover head-vector service2-image ">
       <Header />
       <div className="w-full px-2 md:px-6 lg:px-[118px] flex sm:pt-[80px]">
         <div className="flex flex-col space-y-[30px] py-[80px] items-center justify-center w-full">
-          <h2 className="header-title">Secure Development Tools</h2>
+          <h2 className="text-center header-title">Secure Development Tools</h2>
           <h4 className="font-[16px] text-center">
             A cutting-edge software to assess and enhance the security of your
             code, ensuring your applications are built with security in mind.
