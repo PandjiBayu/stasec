71df0ec3e5b50fb65ef025058f5c6f3d16d9f56e
Pascal <pascaladnan101@gmail.com>
2023-12-11 13:05:02 +0700

    remove space in hero homepage description

--- src/components/banner-homepage/banner.js
+++ src/components/banner-homepage/banner.js
@@ -6,10 +6,10 @@ const { default: HeadVector } = require("../icons/head-vector");
 const Banner = () => {
   return (
     <div class="head-vector background-image bg-cover h-screen relative">
-      <div className="top-0 left-0 hidden xl:block lg:absolute">
+      <Header />
+      <div className="top-0 left-0 hidden xl:block lg:absolute" style={{ zIndex: 0 }}>
         <HeadVector />
       </div>
-      <Header />
       <div className="w-full px-0 md:px-[118px] flex flex-col md:flex-row pt-[80px] ">
         <div className="flex flex-col space-y-[40px] md:py-[80px] w-full items-center md:items-start">
           <div className="text-center md:text-left header-title">
@@ -17,8 +17,8 @@ const Banner = () => {
           </div>
           <h2 className="text-center md:w-full md:text-left font-[16px] md:font-[18px]">
             Guiding you to a path of top-notch cybersecurity. We are devoted to
-            shielding <br /> your data and systems. With STASEC, it&apos;s not
-            just software; it&apos;s a journey to a more <br /> secure digital
+            shielding <br className="hidden md:block"/> your data and systems. With STASEC, it&apos;s not
+            just software; it&apos;s a journey to a more <br className="hidden md:block"/> secure digital
             future.
           </h2>
           <Link href={"/contact"} className="get-started">
