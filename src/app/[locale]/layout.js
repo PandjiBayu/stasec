26bb4943c21f3c49087c5910d75935ae0e82f160
PandjiBayu <pandjibayusatria@gmail.com>
2024-10-07 13:26:31 +0800

    add favicon

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -13,6 +13,12 @@ export default function RootLayout({ children }) {
   return (
     <>
       <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <title>{metadata.title}</title>
+          <meta name="description" content={metadata.description} />
+          <link rel="icon" href="/favicon-1.png" type="image/x-icon" />
+        </head>
         <body className={inter.className}>{children}</body>
       </html>
     </>
